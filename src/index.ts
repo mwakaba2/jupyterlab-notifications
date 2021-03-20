67f1ad94cb7eb5e2d7346279c0aac9c4e59452d4
Mariko Wakabayashi <mariko@mwkby.com>
2021-03-20 12:31:24 -0600

    fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,20 @@ const extension: JupyterFrontEndPlugin<void> = {
   optional: [ISettingRegistry],
   activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry) => {
     checkBrowserNotificationSettings();
-    var enabled = false;
-    var minimumCellExecutionTime = 60;
-    var reportCellExecutionTime = true;
-    var reportCellNumber = true;
+    let enabled = false;
+    let minimumCellExecutionTime = 60;
+    let reportCellExecutionTime = true;
+    let reportCellNumber = true;
     if (settingRegistry) {
       const setting = await settingRegistry.load(extension.id);
       const updateSettings = (): void => {
         enabled = setting.get('enabled').composite as boolean;
-        minimumCellExecutionTime = setting.get('minimum_cell_execution_time').composite as number;
-        reportCellExecutionTime = setting.get('report_cell_execution_time').composite as boolean;
-        reportCellNumber = setting.get('report_cell_number').composite as boolean;
+        minimumCellExecutionTime = setting.get('minimum_cell_execution_time')
+          .composite as number;
+        reportCellExecutionTime = setting.get('report_cell_execution_time')
+          .composite as boolean;
+        reportCellNumber = setting.get('report_cell_number')
+          .composite as boolean;
       };
       updateSettings();
       setting.changed.connect(updateSettings);
@@ -40,12 +43,19 @@ const extension: JupyterFrontEndPlugin<void> = {
             {},
             metadata.get('execution') as any
           );
-          const cellStartTime = new Date(executionMetadata['shell.execute_reply.started']);
-          const cellEndTime = new Date(executionMetadata['shell.execute_reply']);
+          const cellStartTime = new Date(
+            executionMetadata['shell.execute_reply.started']
+          );
+          const cellEndTime = new Date(
+            executionMetadata['shell.execute_reply']
+          );
           const diff = new Date(<any>cellEndTime - <any>cellStartTime);
           if (diff.getSeconds() >= minimumCellExecutionTime) {
-            var notificationPayload = {"icon": "/static/favicon.ico", "body": ""};
-            var message = "";            
+            const notificationPayload = {
+              icon: '/static/favicon.ico',
+              body: ''
+            };
+            let message = '';
             const cellDuration = diff.toISOString().substr(11, 8);
             const cellNumber = notebook.activeCellIndex;
             if (reportCellNumber && reportCellExecutionTime) {
@@ -59,8 +69,10 @@ const extension: JupyterFrontEndPlugin<void> = {
             new Notification('Notebook Cell Completed!', notificationPayload);
           }
         } else {
-          alert('Notebook Cell Timing needs to be enabled for Jupyterlab Notifications to work. ' + 
-                'Please go to Settings -> Advanced Settings Editor -> Notebook and update setting to {"recordTiming": true}');
+          alert(
+            'Notebook Cell Timing needs to be enabled for Jupyterlab Notifications to work. ' +
+              'Please go to Settings -> Advanced Settings Editor -> Notebook and update setting to {"recordTiming": true}'
+          );
         }
       }
     });
--- src/settings.ts
+++ src/settings.ts
@@ -1,30 +1,32 @@
 function checkNotificationPromise(): boolean {
   try {
     Notification.requestPermission().then();
-  } catch(e) {
+  } catch (e) {
     return false;
   }
   return true;
 }
-  
+
 function handlePermission(permission: string): void {
-  if (permission == 'granted') {
-    alert("Browser Notifications are allowed. (^_^)b");
+  if (permission === 'granted') {
+    alert('Browser Notifications are allowed. (^_^)b');
   } else {
-    alert("Browser Notifications are not allowed. Please update your browser settings to allow notifications.");
+    alert(
+      'Browser Notifications are not allowed. Please update your browser settings to allow notifications.'
+    );
   }
 }
 
 export function checkBrowserNotificationSettings(): void {
   if (!('Notification' in window)) {
-    alert("This browser does not support notifications.");
+    alert('This browser does not support notifications.');
   } else if (Notification.permission !== 'granted') {
-    if(checkNotificationPromise()) {
-      Notification.requestPermission().then((permission) => {
+    if (checkNotificationPromise()) {
+      Notification.requestPermission().then(permission => {
         handlePermission(permission);
       });
     } else {
-      Notification.requestPermission(function(permission) {
+      Notification.requestPermission(permission => {
         handlePermission(permission);
       });
     }
